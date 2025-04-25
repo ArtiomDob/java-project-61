bce6784996fd38b0ac529d29649c23e02c15043f
Artem Dobrotvorskiy <hyeri@bk.ru>
2025-04-25 17:34:56 +0300

    were

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Cli {
 
-  public static void welcome() {
-      System.out.print("Welcome to the Brain Games! ");
+    public static void welcome() {
+        System.out.print("Welcome to the Brain Games! ");
         Scanner userInputScanner = new Scanner(System.in);
        // System.out.print("Welcome to the Brain Games! ");
         System.out.print("May I have your name? ");
