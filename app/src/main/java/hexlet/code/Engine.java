66f0200cfc3fdb0091344d378cab2e554a2dcbe4
ArtiomDob <hyeri@bk.ru>
2025-06-03 00:53:07 +0300

    were

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -64,6 +64,7 @@ import java.util.Scanner;
 public class Engine {
 
     private static final int ROUNDS_COUNT = 3;
+    private static int correctAnswersCount = 0;
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void runGame(String gameName, Scanner scanner) {
@@ -71,6 +72,7 @@ public class Engine {
         System.out.print("May I have your name? ");
         String name = scanner.nextLine().trim();
         System.out.println("Hello, " + name + "!");
+        // private static int correctAnswersCount = 0;
 
         switch (gameName) {
             case "even":
@@ -79,30 +81,49 @@ public class Engine {
             case "calc":
                 hexlet.code.games.Calculator.start(scanner, name);
                 break;
-          //  case "gcd":
+            //  case "gcd":
             //    hexlet.code.games.GCD.start(scanner, name);
             //    break;
             default:
                 System.out.println("Unknown game");
                 return;
         }
+        // if (correctAnswersCount >= ROUNDS_COUNT) {
+        // System.out.println("Congratulations, " + name + "!");
+        //System.out.println("Congratulations, " + name + "!");
 
-       // System.out.println("Congratulations, " + name + "!");
+    }
 
-        }
+    //  public static void examination(String userAnswer, String correctAnswerStr, String userName) {// проверяем
+    //     if (userAnswer.equals(correctAnswerStr)) {//сравниваем
+//            System.out.println("Correct!");
+    //       correctAnswersCount++;
+    //        if (correctAnswersCount >= ROUNDS_COUNT) {
+    //           System.out.println("Congratulations, " + userName + "!");
+    //System.out.println("Congratulations, " + name + "!");// если правильно
+    //       } else {
+    //         System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswerStr + "'.");// если не правильно
+    //         System.out.println("Let's try again, " + userName + "!");
+    //        return;
+    //     }
+    //System.out.println("Congratulations, " + userName + "!");
+    // }
+    // }
+//}
 
-        public static void examination(String userAnswer, String correctAnswerStr, String userName) {// проверяем
-        if (userAnswer.equals(correctAnswerStr)) {//сравниваем
+    public static void examination(String userAnswer, String correctAnswerStr, String userName) {
+        if (userAnswer.equals(correctAnswerStr)) {
             System.out.println("Correct!");
-            //System.out.println("Congratulations, " + name + "!");// если правильно
+            correctAnswersCount++;
+            if (correctAnswersCount >= ROUNDS_COUNT) {
+                System.out.println("Congratulations, " + userName + "!");
+            }
         } else {
-            System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswerStr + "'.");// если не правильно
+            System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswerStr + "'.");
             System.out.println("Let's try again, " + userName + "!");
-            return;
         }
     }
 }
-
 
 
 
