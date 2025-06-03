9669f7771bce4c53945e3da47100a4005de2438b
ArtiomDob <hyeri@bk.ru>
2025-06-03 12:08:21 +0300

    were

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,58 +1,3 @@
-/*package hexlet.code;
-
-import java.util.Scanner;
-
-public class Engine {
-    public static String getUserName(Scanner scanner) {
-        System.out.print("May I have your name? ");
-        return scanner.nextLine();
-    }
-}  */
-
-/*package hexlet.code;
-
-import java.util.Scanner;
-
-public class Engine {
-
-    private static final int ROUNDS_COUNT = 3;
-    private static final Scanner SCANNER = new Scanner(System.in);
-    public static void runGame(String gameName, Scanner scanner) {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String name = scanner.nextLine().trim();
-        System.out.println("Hello, " + name + "!");
-
-        {
-            boolean correct = false;
-            switch (gameName) {
-                case "even":
-                    hexlet.code.games.Even.start(scanner, name);
-                    break;
-                case "calc":
-                    hexlet.code.games.Calculator.start(scanner, name);
-                    break;
-                 case "gcd":
-                    hexlet.code.games.GCD.start(scanner, name);
-                   break;
-
-                default:
-                    System.out.println("Unknown game");
-                    return;
-            }
-            if (!correct) {
-                System.out.println("Let's try again, " + name + "!");
-                return;
-            }
-        }
-
-        System.out.println("Congratulations, " + name + "!");
-    }
-}
-
- */
-
-
 
 package hexlet.code;
 
@@ -72,7 +17,6 @@ public class Engine {
         System.out.print("May I have your name? ");
         String name = scanner.nextLine().trim();
         System.out.println("Hello, " + name + "!");
-        // private static int correctAnswersCount = 0;
 
         switch (gameName) {
             case "even":
@@ -86,30 +30,10 @@ public class Engine {
             //    break;
             default:
                 System.out.println("Unknown game");
-               // return;
+
         }
-        // if (correctAnswersCount >= ROUNDS_COUNT) {
-        // System.out.println("Congratulations, " + name + "!");
-        //System.out.println("Congratulations, " + name + "!");
 
     }
-
-    //  public static void examination(String userAnswer, String correctAnswerStr, String userName) {// проверяем
-    //     if (userAnswer.equals(correctAnswerStr)) {//сравниваем
-//            System.out.println("Correct!");
-    //       correctAnswersCount++;
-    //        if (correctAnswersCount >= ROUNDS_COUNT) {
-    //           System.out.println("Congratulations, " + userName + "!");
-    //System.out.println("Congratulations, " + name + "!");// если правильно
-    //       } else {
-    //         System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswerStr + "'.");// если не правильно
-    //         System.out.println("Let's try again, " + userName + "!");
-    //        return;
-    //     }
-    //System.out.println("Congratulations, " + userName + "!");
-    // }
-    // }
-//}
 
     public static void examination(String userAnswer, String correctAnswerStr, String userName) {// проверяем
         if (userAnswer.equals(correctAnswerStr)) {//сравниваем
